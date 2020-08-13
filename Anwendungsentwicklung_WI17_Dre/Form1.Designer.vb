2de8a949981cdf1c2cb089abe61f2a90918777ed
Robin Genz <mail@robingenz.de>
2020-08-13 12:16:48 +0200

    feat: add labels

--- Anwendungsentwicklung_WI17_Dre/Form1.Designer.vb
+++ Anwendungsentwicklung_WI17_Dre/Form1.Designer.vb
@@ -23,6 +23,9 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Rechne = New System.Windows.Forms.Button()
+        Me.FormulaLabel = New System.Windows.Forms.Label()
+        Me.ResultLabel = New System.Windows.Forms.Label()
+        Me.ResultValueLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Rechne
@@ -34,11 +37,44 @@ Partial Class Form1
         Me.Rechne.Text = "Rechne"
         Me.Rechne.UseVisualStyleBackColor = True
         '
+        'FormulaLabel
+        '
+        Me.FormulaLabel.AutoSize = True
+        Me.FormulaLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormulaLabel.Location = New System.Drawing.Point(24, 60)
+        Me.FormulaLabel.Name = "FormulaLabel"
+        Me.FormulaLabel.Size = New System.Drawing.Size(51, 17)
+        Me.FormulaLabel.TabIndex = 1
+        Me.FormulaLabel.Text = "Formel"
+        '
+        'ResultLabel
+        '
+        Me.ResultLabel.AutoSize = True
+        Me.ResultLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ResultLabel.Location = New System.Drawing.Point(24, 98)
+        Me.ResultLabel.Name = "ResultLabel"
+        Me.ResultLabel.Size = New System.Drawing.Size(64, 17)
+        Me.ResultLabel.TabIndex = 2
+        Me.ResultLabel.Text = "Ergebnis"
+        '
+        'ResultValueLabel
+        '
+        Me.ResultValueLabel.AutoSize = True
+        Me.ResultValueLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ResultValueLabel.Location = New System.Drawing.Point(137, 98)
+        Me.ResultValueLabel.Name = "ResultValueLabel"
+        Me.ResultValueLabel.Size = New System.Drawing.Size(17, 17)
+        Me.ResultValueLabel.TabIndex = 3
+        Me.ResultValueLabel.Text = "0"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(361, 141)
+        Me.Controls.Add(Me.ResultValueLabel)
+        Me.Controls.Add(Me.ResultLabel)
+        Me.Controls.Add(Me.FormulaLabel)
         Me.Controls.Add(Me.Rechne)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
@@ -46,8 +82,12 @@ Partial Class Form1
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Formel-Interpreter"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Rechne As Button
+    Friend WithEvents FormulaLabel As Label
+    Friend WithEvents ResultLabel As Label
+    Friend WithEvents ResultValueLabel As Label
 End Class
