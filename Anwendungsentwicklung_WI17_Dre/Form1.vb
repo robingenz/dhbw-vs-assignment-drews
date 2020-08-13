ca1ce9ccec328ce83dcfa5bdc0c71c134201e73f
Robin Genz <mail@robingenz.de>
2020-08-13 14:50:47 +0200

    chore: remove ResultValueLabel_Click sub

--- Anwendungsentwicklung_WI17_Dre/Form1.vb
+++ Anwendungsentwicklung_WI17_Dre/Form1.vb
@@ -2,8 +2,4 @@
     Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
 
     End Sub
-
-    Private Sub ResultValueLabel_Click(sender As Object, e As EventArgs) Handles ResultValueLabel.Click
-
-    End Sub
 End Class
